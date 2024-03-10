c60b382fb79f058d8aa40fce68bd5294a3a8ab24
Nikesh Bhandari <bhandarinikesh93@gmail.com>
2024-03-10 16:03:00 +0545

    Updated as of sandesh

--- floydwarshall.java
+++ floydwarshall.java
@@ -1,54 +1,33 @@
 class FloydWarshall{
-    final static int INF= 99999, V= 4;
+    final static int INF= 9999, V= 4;
 
     void FloydWarshall( int dist[][]){
         int i, j, k;
         for(k=0; k<V; k++){
             for(i=0; i<V; i++){
                 for(j=0; j<V; j++){
-                    // if(dist[i][j]> dist[i][k]+dist[k][j]){
-                    //     dist[i][j] = dist[i][k]+dist[k][j];
-                    // }
-                    dist[i][j]=Math.min(dist[i][j], dist[i][k]+dist[k][j]);
-
+                    dist[i][j]= Math.min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
-        // printSolution(dist);
-        for(int x=0;x<V;x++){
-            for (int y=0;y<V;y++){
-                System.out.print(dist[x][y]+" ");
+        for(i=0; i<V; i++){
+            for(j=0; j<V; j++){
+                System.out.print(dist[i][j] + " ");
             }
             System.out.println();
         }
     }
-
-    void printSolution(int dist[][]){
-        System.out.println("Ouptut");
-        for(int i=0; i<V; ++i){
-            for(int j=0; j<V; ++j){
-                if(dist[i][j]==INF){
-                    System.out.print("INF ");
-                }
-                else{
-                    System.out.print(dist[i][j] + " ");
-                }
-                
-            }
-            System.out.println();
-        }
-
-    }
-
     public static void main(String[] args){
-        int graph[][] = {{ 0, 8, INF, 1 },
-                        { INF, 0, 1, INF },
+        int graph[][] = {{ 0, 2, INF, 4 },
+                        { INF, 0, 6, INF },
                         { 4, INF, 0, INF },
-                        { INF, 2, 9, 0 }};
+                        { INF, 7, 1, 0 }};
         FloydWarshall f= new FloydWarshall();
+
         f.FloydWarshall(graph);
     }
 
 }
+
 //time complexity = O(n^3)
 //space complexity = O(2n^2)
